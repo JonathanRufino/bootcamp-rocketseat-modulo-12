911ce4494af7d85c464ef9f0b63a4e19deb1e90b
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-08-08 23:11:21 -0300

    test(TechList): save techs in localStorage

--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // import { Container } from './styles';
 
 export default function TechList() {
   const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState('');
+
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs');
+
+    if (storageTechs) {
+      setTechs(JSON.parse(storageTechs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
 
   function handleAddTech() {
     setTechs([...techs, newTech]);
